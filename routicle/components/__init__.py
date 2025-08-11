fb4c50205ffe522fcdf441bdf16a43982f520f84
E33605 <debmalyapramanik.005@gmail.com>
2025-08-11 12:35:47 +0530

    ✨ create base class for different types of nodes

--- routicle/components/__init__.py
+++ routicle/components/__init__.py
@@ -11,3 +11,4 @@ abstract pydantic validator class.
 """
 
 from routicle.components import base # noqa: F401, F403
+from routicle.components import nodes # noqa: F401, F403
---
+++ routicle/components/nodes.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+
+"""
+Defination of a Node Attributes for a Gaph
+
+A node for supply chain and logistics problem is subclassified into
+multiple types based on requirement. The definaion and attributes are
+defined. The attributes are typical constraints like supply/demand for
+a particular node type, for example a plant or an warehouse.
+"""
+
+from routicle.components.base import GraphNode
+
+class NTPorts(GraphNode):
+    """
+    Node Type:: Port - A Location for Incoming Material
+
+    A node (sea, air, rail, etc.) is a dedicated location of material
+    import and/or export. Typically, a vendor can dump material at a
+    port and based on inland charges the material can then be
+    transported to/from another location.
+    """
+
+    @property
+    def color(self) -> str:
+        return "#FDE725"
+
+
+class NTWarehouses(GraphNode):
+    """
+    Node Type:: Warehouses - A Location typically for Storage
+
+    A warehouse is a location for storing raw materials and/or finished
+    goods. An warehouse can have many constraints like some materials
+    cannot be stored together. Currently, the model is kept simple
+    with only capacity constraint however other attributes maybe added
+    in a future version.
+    """
+
+    @property
+    def color(self) -> str:
+        return "#5EC962"
+
+
+class NTPlants(GraphNode):
+    """
+    Node Type:: Plants - A Location for Manufaturing
+
+    A plant is a location of manufacturing. In addition to storage
+    capacity a plant must have a production constraint.
+
+    :ivar rate: Production rate, which should be in the same unit as
+        that of the capacity, per day.
+    """
+
+    rate : float
+
+    @property
+    def color(self) -> str:
+        return "#3B528B"
+
+
+class NTVendors(GraphNode):
+    """
+    Node Type:: Vendors - An Entity who can Supply Material
+
+    A vendor can supply material to a location (ports, plants, etc.).
+    A vendor's lead time is most of the route specific so this must be
+    defined as a weight (``edges`` attributes).
+    """
+
+    @property
+    def color(self) -> str:
+        return "#440154"
