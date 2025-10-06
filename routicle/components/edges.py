8fa7aa5d89c56e2f9ddba6f0b1f1c92d226d4294
E33605 <debmalyapramanik.005@gmail.com>
2025-10-06 14:02:59 +0530

    ⚙️🌟 allow all components to allow arbitary arguments
    - create two ned edges - time based and cost based (only)

--- routicle/components/edges.py
+++ routicle/components/edges.py
@@ -8,6 +8,66 @@ from pydantic import ConfigDict
 
 from routicle.components.base import POIConnector
 
+class TimeEdge(POIConnector):
+    """
+    An Edge (connection b/w nodes) with a Time Parameter
+
+    An edge where time is required to be minimized, is defined as a
+    time based edge and typically for a supply chain problem this
+    factor needs to be minimized.
+
+    :ivar time: Time required for the transport, this is typically a
+        factor which requires minimization.
+    """
+
+    time : float
+
+
+    @property
+    def weight(self) -> float:
+        return self.time
+
+
+    @property
+    def attributes(self) -> dict:
+        return dict(
+            time = self.time,
+            color = self.color,
+            weight = self.weight
+        )
+
+
+class CostEdge(POIConnector):
+    """
+    An Edge (connection b/w nodes) with a Cost Parameter
+
+    An edge where cost is required to be minimized, is defined as a
+    cost based edge and typically for a supply chain problem this
+    factor needs to be minimized.
+
+    :ivar cost: The cost required, example inland freight, handling
+        cost etc. for the transport. This is also typically requires a
+        minimization approach.
+    """
+
+    cost : float
+
+
+    @property
+    def weight(self) -> float:
+        return self.cost
+
+
+    @property
+    def attributes(self) -> dict:
+        return dict(
+            time = self.time,
+            color = self.color,
+            weight = self.weight
+        )
+
+
+
 class TimeCostEdge(POIConnector):
     """
     An Edge (connection b/w nodes) with a Time & Cost Parameter
